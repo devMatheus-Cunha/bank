1fe06b218202863065a2d19ec53c4b1a1aa117d9
Matheus Gomes <gomesprogramacao@gmail.com>
2021-09-26 13:48:19 -0300

    feat: Container initial userContent

---
+++ src/container/UserContent/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+// components
+import InfoUserContent from "../../components/InfoUserContent";
+
+// styles
+import { Container } from "./styles";
+
+const UserContent: React.FC = () => {
+  return (
+    <Container>
+      <InfoUserContent />
+    </Container>
+  );
+};
+
+export default UserContent;
