3b13e064181103cae2fbb242b8d8070c5038085f
Matheus Gomes <gomesprogramacao@gmail.com>
2021-09-26 14:36:55 -0300

    feat: add handleActions

--- src/container/UserContent/index.tsx
+++ src/container/UserContent/index.tsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 // components
 import InfoUserContent from "../../components/InfoUserContent";
+import ActionsBox from "../../components/ActionsBox";
 
 // styles
 import { Container } from "./styles";
 
 const UserContent: React.FC = () => {
+  // function handle
+  const handleTranferWallet = useCallback(() => {
+    console.log("Transefeir");
+  }, []);
+
+  const handleDepositWallet = useCallback(() => {
+    console.log("Depositar");
+  }, []);
+
   return (
     <Container>
       <InfoUserContent />
+      <ActionsBox
+        handleTranferWallet={handleTranferWallet}
+        handleDepositWallet={handleDepositWallet}
+      />
     </Container>
   );
 };
