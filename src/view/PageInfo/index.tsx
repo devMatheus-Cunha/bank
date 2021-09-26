48551fc46df90907240963714691a1a6eb97b6f9
Matheus Gomes <gomesprogramacao@gmail.com>
2021-09-26 13:12:43 -0300

    feat: new sctructure with UserContent in file view

---
+++ src/view/PageInfo/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import UserContent from '../../container/UserContent';
+
+
+const PageInfo: React.FC = () => {
+  return (
+    <UserContent />
+  );
+}
+
+export default PageInfo;
