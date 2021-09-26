6c8bcc963f113c4935755fa6d88b3ac82f141bde
Matheus Gomes <gomesprogramacao@gmail.com>
2021-09-26 14:05:16 -0300

    fix: style base InfoUserContent

--- src/components/InfoUserContent/styles.ts
+++ src/components/InfoUserContent/styles.ts
@@ -1,21 +1,30 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  background-color: var(--background);
   padding: 1rem;
+  margin: 0.8rem;
+  border-radius: 1rem;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 export const ContentInfo = styled.div`
   h2 {
-    color: var(--white);
+    color: var(--text-title);
     margin-bottom: 1.4rem;
   }
 `;
 export const ContentBalance = styled.div`
   h1 {
-    margin-bottom: 0.5rem;
-    color: var(--white);
+    margin-bottom: 0.2rem;
+    color: var(--text-title);
+    font-size: 17px;
   }
   h2 {
-    color: var(--white);
+    color: var(--text-title);
+    font-size: 20px;
   }
 `;
