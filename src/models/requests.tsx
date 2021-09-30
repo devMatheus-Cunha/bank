68d62c13863571718f76b0ca10cfd71cb4d3556d
Matheus Gomes <gomesprogramacao@gmail.com>
2021-09-30 18:25:21 -0300

    feat: add route PostTrasnfer

--- src/models/requests.tsx
+++ src/models/requests.tsx
@@ -1,11 +1,17 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://3b4a-2804-14c-5b80-80b4-a1f6-7d66-967e-642.ngrok.io",
+  baseURL: "http://506e-2804-14c-5b80-80b4-a1f6-7d66-967e-642.ngrok.io",
 });
 
 export const PostDeposit = (id: string, data: any) => {
   api.post(`/picpay/deposit/save/${id}`, data).then((response) => {
+    window.location.reload();
+  });
+};
+
+export const PostTransfer = (id: string, data: any) => {
+  api.post(`/picpay/transactions/${id}`, data).then((response) => {
     window.location.reload();
   });
 };
