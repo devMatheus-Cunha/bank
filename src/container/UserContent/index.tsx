c06a3b5987decc85abb62e7b44a1c6da11cf2efa
Matheus Gomes <gomesprogramacao@gmail.com>
2021-09-29 22:11:59 -0300

    feat: requisiton in the back for dataUserId

--- src/container/UserContent/index.tsx
+++ src/container/UserContent/index.tsx
@@ -1,7 +1,5 @@
-import React  from "react";
-
-// hooks
-import DataUser from "../../hooks/userData";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 // components
 import InfoUserContent from "../../components/InfoUserContent";
@@ -10,11 +8,22 @@ import InfoUserContent from "../../components/InfoUserContent";
 import { Container } from "./styles";
 
 const UserContent: React.FC = () => {
-  const {name, wallet} = DataUser()
+  const [state, setState] = useState<any>();
+  const api = axios.create({
+    baseURL: "http://7aa1-2804-14c-5b80-80b4-f050-4dc1-95a5-8c5e.ngrok.io",
+  });
 
+  useEffect(() => {
+    api
+      .get("/picpay/admin/user/614e2c38f49bb04a582ad344")
+      .then((response) => setState(response.data))
+      .catch((err) => {
+        console.error("ops! ocorreu um erro" + err);
+      });
+  }, []);
   return (
     <Container>
-      <InfoUserContent name={name} wallet={wallet}/>
+      <InfoUserContent name={state?.complete_name} wallet={state?.wallet} />
     </Container>
   );
 };
