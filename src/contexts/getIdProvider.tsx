49f305479b754c30c4d9146f87b632225fa64e29
Matheus Gomes <gomesprogramacao@gmail.com>
2021-09-30 19:58:48 -0300

    feat: apllying types

--- src/contexts/getIdProvider.tsx
+++ src/contexts/getIdProvider.tsx
@@ -2,35 +2,31 @@
 import { createContext, useEffect, useState } from "react";
 import axios from "axios";
 
-export const GetIdContext = createContext({} as any);
+// interface
+import { IDataProps, IGetIdProvider, IUserDataProps } from "../interface";
 
+// context
+export const GetIdContext = createContext({} as IUserDataProps);
+
+// url api
 const api = axios.create({
   baseURL: "http://506e-2804-14c-5b80-80b4-a1f6-7d66-967e-642.ngrok.io",
 });
 
-const GetIdProvider = (props: any) => {
-  const [userData, setUserData] = useState<any>({
-    data: {},
-    id: "6156223abe4fe81d80da773d",
-  });
+const GetIdProvider = ({ children }: IGetIdProvider) => {
+  const [userData, setUserData] = useState<IDataProps>();
+  const [userId] = useState("6156223abe4fe81d80da773d");
   // const { id } = useParams<any>();
 
   useEffect(() => {
-    api.get(`/picpay/admin/user/${userData.id}`).then((response) => {
-      setUserData({
-        ...userData,
-        data: response.data,
-      });
+    api.get(`/picpay/admin/user/${userId}`).then((response) => {
+      setUserData(response.data);
     });
-  }, [userData.id]);
+  }, [userId]);
 
   return (
-    <GetIdContext.Provider
-      value={{
-        userData,
-      }}
-    >
-      {props.children}
+    <GetIdContext.Provider value={{ userData, userId } as IUserDataProps}>
+      {children}
     </GetIdContext.Provider>
   );
 };
--- src/models/requests.tsx
+++ src/models/requests.tsx
@@ -1,16 +1,20 @@
 import axios from "axios";
 
+// interface
+import { IValuesTransferProps } from "../interface";
+
+// url api
 const api = axios.create({
   baseURL: "http://506e-2804-14c-5b80-80b4-a1f6-7d66-967e-642.ngrok.io",
 });
 
-export const PostDeposit = (id: string, data: any) => {
+export const PostDeposit = (id: string, data: number) => {
   api.post(`/picpay/deposit/save/${id}`, data).then((response) => {
     window.location.reload();
   });
 };
 
-export const PostTransfer = (id: string, data: any) => {
+export const PostTransfer = (id: string, data: IValuesTransferProps) => {
   api.post(`/picpay/transactions/${id}`, data).then((response) => {
     window.location.reload();
   });
