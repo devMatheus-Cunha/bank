196c979299db5dd47abb84165d80f026bd32721e
Matheus Gomes <gomesprogramacao@gmail.com>
2021-09-30 18:24:49 -0300

    feat: handleTransfer with backend

--- src/container/UserContent/items/transfer/index.tsx
+++ src/container/UserContent/items/transfer/index.tsx
@@ -1,22 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
 // styles
 import { Container, Content, ContentButton } from "../shared/styles";
 
 interface ITransferProps {
-  handleSubmit: () => void;
+  handleSubmit: (values: IValuesTransferProps) => void;
   onCloseModal: () => void;
 }
 
+interface IValuesTransferProps {
+  toId: string;
+  value: string;
+}
+
 const Transfer = ({ handleSubmit, onCloseModal }: ITransferProps) => {
+  const [valuesTransfer, setValuesTransfer] = useState<IValuesTransferProps>({
+    toId: "",
+    value: "",
+  });
+
   return (
     <Container>
       <Content>
-        <input type="text" placeholder="Digite o pix..." />
-        <input type="number" placeholder="Valor..." />
+        <input
+          type="text"
+          name="id"
+          placeholder="Digite o pix..."
+          onChange={(event) =>
+            setValuesTransfer({
+              ...valuesTransfer,
+              toId: event.target.value,
+            })
+          }
+        />
+        <input
+          type="number"
+          name="value"
+          placeholder="Valor..."
+          onChange={(event) =>
+            setValuesTransfer({
+              ...valuesTransfer,
+              value: event.target.value,
+            })
+          }
+        />
         <ContentButton>
           <button onClick={() => onCloseModal()}>Cancelar</button>
-          <button onClick={() => handleSubmit()}>Transferir</button>
+          <button onClick={() => handleSubmit(valuesTransfer)}>Transferir</button>
         </ContentButton>
       </Content>
     </Container>
