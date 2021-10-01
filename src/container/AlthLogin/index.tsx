e69576201ec0f573c701bf7da4cf133d9b914bcd
bernardo-mr1 <devbernardomr@gmail.com>
2021-09-30 21:56:49 -0300

    feat: :sparkles: Create layout login

---
+++ src/container/AlthLogin/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import { Container, Form, ContentInput, ContentTitleLogin } from "./styles";
+
+const AlthLogin: React.FC = () => {
+  return (
+    <>
+      <Container>
+        <ContentTitleLogin>
+          <h1 className="user-login-title">Login</h1>
+        </ContentTitleLogin>
+        <Form>
+          <ContentInput>
+            <label htmlFor="user">User</label>
+            <input type="text" name="user" placeholder="E-mail" />
+          </ContentInput>
+          <ContentInput>
+            <label htmlFor="user">Password</label>
+            <input type="password" name="password" placeholder="Password" />
+          </ContentInput>
+        </Form>
+      </Container>
+    </>
+  );
+};
+
+export default AlthLogin;
---
+++ src/container/AlthLogin/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+ 
+`;
+export const ContentTitleLogin = styled.div`
+h1 {
+    color: blue;
+    text-align: center;
+}
+
+`;
+export const Form = styled.form`
+  
+`;
+export const ContentInput = styled.div`
+  
+`;
+
