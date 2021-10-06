760861ffb374b45c7fb7c2f1360f100a9d579fae
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-06 11:14:54 -0300

    refactor: applying new models for useDataUser

--- src/hooks/useDataUser.ts
+++ src/hooks/useDataUser.ts
@@ -1,24 +1,19 @@
 import { createContext, useEffect, useState } from "react";
-import axios from "axios";
 
 // interface
 import { IDataProps, IUserDataProps } from "../interface";
+import { ModelGet } from "../models/modelGet";
 
 // context
 export const GetIdContext = createContext({} as IUserDataProps);
-
-// url api
-const api = axios.create({
-	baseURL: "http://ade5-2804-14c-5b80-80b4-a1f6-7d66-967e-642.ngrok.io",
-});
 
 const useDataUser = (id: string) => {
 	const [userData, setUserData] = useState<IDataProps>();
 
 	useEffect(() => {
-		api.get(`/picpay/admin/user/${id}`).then((response) => {
-			setUserData(response.data);
-		});
+		ModelGet({
+			route: `/picpay/admin/user/${id}`,
+		}).then((response) => setUserData(response))
 	}, [id]);
 
 	return { userData };
