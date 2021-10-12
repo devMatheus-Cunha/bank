4da838f3cf0b930145a5e6752131c3b017f76372
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-12 00:02:44 -0300

    fix: style useContent

--- src/container/UserContent/styles.ts
+++ src/container/UserContent/styles.ts
@@ -8,31 +8,11 @@ export const ContainerActionsButtons = styled.div`
 
 export const ContentButton = styled.div`
   display: flex;
-  flex-direction:column;
+  flex-direction:row;
   align-items: center;
   justify-content: center;
-  background-color: var(--blue-light);
 
-  width: 50%;
-  height: 70px;
-  border-radius: 2rem;
+  gap: 1rem;
+  width: 100%;
   margin-top: 1rem;
-
-  svg {
-    width: 23px;
-    height: 23px;
-  }
-
-  > button {
-    outline: none;
-    border: none;
-    color: var(--white);
-  }
-
-  p {
-    font-size: 15px;
-    letter-spacing: 0.1rem;
-    font-family: 'Poppins', sans-serif;
-    color: var(--white);
-  }
 `;
