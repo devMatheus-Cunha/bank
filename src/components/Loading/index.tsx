a08573bf7ee4a9dbb9f4ab23bc56364ef7c7261c
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-12 00:00:19 -0300

    feat: component Loading

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import {
+	 Loader,
+} from "semantic-ui-react"
+
+// styles
+import { Container } from "./styles"
+
+const LoaderExampleLoader = () => (
+	<Container>
+		<Loader active inline="centered" size="medium" />
+	</Container>
+)
+
+export default LoaderExampleLoader
