e85243504c6d17103fb356fb605b52343199369a
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-18 18:23:31 -0300

    feat: add funciton handleGetPDF in the utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
-export const url = "http://91aa-2804-14c-5b80-80b4-f1f0-a52f-5dab-38f6.ngrok.io"
+export const url = "http://d0a9-201-17-210-95.ngrok.io"
 
 export const headers = {
 	Accept: "application/json",
@@ -6,3 +6,19 @@ export const headers = {
 }
 
 export const mensageErrorDefault = "Erro! Tente novamente mais tarde..."
+
+export const generetePDF = (data: any) => {
+	const url = window.URL.createObjectURL(
+		new Blob([data], { type: "application/pdf" }),
+	);
+	const link = document.createElement("a");
+	link.href = url;
+	window.open(url, "_blank");
+
+	// ? reserve
+	// link.setAttribute("download", "lista_etiquetas.pdf");
+	// document.body.appendChild(link);
+	// link.click();
+}
+
+export const pdfConfig = "arraybuffer";
