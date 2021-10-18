f6bfec29fb7134c9e701d09e53b5921b51fb95d8
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-18 20:50:10 -0300

    fix: style container UserContent

--- src/container/UserContent/styles.ts
+++ src/container/UserContent/styles.ts
@@ -1,18 +1,25 @@
 import styled from "styled-components";
 
-export const ContainerActionsButtons = styled.div`
+export const Contaienr = styled.div`
   display: flex;
-  gap: 1rem;
+  flex-direction: column;
+`;
+
+export const ContainerActionsButtons = styled.div`
   margin: 0.8rem;
-  `;
+`;
 
 export const ContentButton = styled.div`
   display: flex;
-  flex-direction:row;
+  flex-direction: row;
   align-items: center;
   justify-content: center;
 
   gap: 1rem;
   width: 100%;
-  margin-top: 1rem;
+`;
+
+export const Title = styled.h3`
+  color: white;
+  padding: 0 1rem;
 `;
