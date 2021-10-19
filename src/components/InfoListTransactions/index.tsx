ae3dea922a91d676ea1ab92bea0edacc469acd33
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-18 21:44:45 -0300

    feat: add types for component infoListTransactions

--- src/components/InfoListTransactions/index.tsx
+++ src/components/InfoListTransactions/index.tsx
@@ -4,61 +4,61 @@ import React from "react";
 import LoaderExampleLoader from "../Loading";
 
 // styles
-import {
-	Container,
-	Content,
-	InfoData,
-} from "./styles";
+import { Container, Content, InfoData } from "./styles";
+
+type DatasProps = {
+  fromWho: string;
+  toSend: string;
+  value: number;
+  date: any;
+};
 
 type InfoListTransactionsAndDepositProps = {
-	loading: boolean,
-	data: any
-}
+  loading: boolean;
+  data: DatasProps[];
+};
 
 const InfoListTransactionsAndDeposit = ({
-	loading, data,
+	loading,
+	data,
 }: InfoListTransactionsAndDepositProps) => {
 	return (
 		<Container>
-			{
-				loading ? (
-					data.map(({
-						fromWho, toSend, date, value,
-					}: any) => (
-						<>
-							<Content>
-								<InfoData>
-									<h4>De quem:</h4>
-									<p>
-										{fromWho}
-									</p>
-								</InfoData>
-								<InfoData>
-									<h4>Para quem:</h4>
-									<p>{toSend}</p>
-								</InfoData>
-								<InfoData>
-									<h4>Data:</h4>
-									<p>
-										{new Intl.DateTimeFormat("pt-BR").format(new Date(date))}
-									</p>
-								</InfoData>
-								<InfoData>
-									<h4>Valor:</h4>
-									<p>
-										{new Intl.NumberFormat("pt-BR", {
-											style: "currency",
-											currency: "BRL",
-										}).format(value as unknown as number)}
-									</p>
-								</InfoData>
-							</Content>
-						</>
-					))
-				) : (
-					<LoaderExampleLoader />
-				)
-			}
+			{loading ? (
+				data.map((datas: DatasProps) => (
+					<>
+						<Content>
+							<InfoData>
+								<h4>De quem:</h4>
+								<p>{datas.fromWho}</p>
+							</InfoData>
+							<InfoData>
+								<h4>Para quem:</h4>
+								<p>{datas.toSend}</p>
+							</InfoData>
+							<InfoData>
+								<h4>Data:</h4>
+								<p>
+									{new Intl.DateTimeFormat("pt-BR").format(
+										new Date(datas.date),
+									)}
+								</p>
+							</InfoData>
+							<InfoData>
+								<h4>Valor:</h4>
+								<p>
+									{new Intl.NumberFormat("pt-BR", {
+										style: "currency",
+										currency: "BRL",
+									}).format(datas.value as unknown as number)}
+								</p>
+							</InfoData>
+						</Content>
+					</>
+				))
+			) : (
+				<LoaderExampleLoader />
+			)}
 		</Container>
 	);
 };
