55b68d2e129f9e1c24ccfba3eb8c0fda922cc521
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-20 20:50:26 -0300

    feat: applying dinamic datas for component InfoListTransactions

--- src/components/InfoListTransactions/index.tsx
+++ src/components/InfoListTransactions/index.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Modal from "react-modal";
 
 // icons
-import { BiDetail } from "react-icons/bi"
+import { BiDetail } from "react-icons/bi";
 
 // items
 import ContainerModalDetails from "./items";
@@ -34,7 +34,7 @@ const InfoListTransactionsAndDeposit = ({
 	loading,
 	datas,
 }: InfoListTransactionsAndDepositProps) => {
-	const [isModal, setisModal] = useState(false)
+	const [isModal, setisModal] = useState(false);
 
 	// handle open modal
 	const handleOpenModaDetails = () => {
@@ -47,46 +47,69 @@ const InfoListTransactionsAndDeposit = ({
 	};
 
 	return (
-		<Container>
-			<ContentFirst>
-				<Title>Transferencia enviada</Title>
-				<Destiny>
-					Para:
-					<span> Matheus</span>
-				</Destiny>
-				<Wallet>
-					{new Intl.NumberFormat("pt-BR", {
-						style: "currency",
-						currency: "BRL",
-					}).format(10 as unknown as number)}
-				</Wallet>
-			</ContentFirst>
-			<ContentSecond>
-				<DateTransaction>
-					{new Intl.DateTimeFormat("pt-BR", {
-						day: "numeric",
-						month: "numeric",
-					}).format(new Date())}
-				</DateTransaction>
-				<Details>
-					<button type="button" onClick={() => handleOpenModaDetails()}>
-						<BiDetail />
-					</button>
-					<Modal
-						isOpen={isModal}
-						onRequestClose={handleCloseModalDetails}
-						overlayClassName="react-modal-overlay"
-						className="react-modal-content"
-					>
-						<ContainerModalDetails
-							// handleSubmit={() => console.log("")}
-							onCloseModal={handleCloseModalDetails}
-						/>
-					</Modal>
-				</Details>
-			</ContentSecond>
-		</Container>
-	);
+		<>
+			{
+				loading && datas && datas.length > 0 ? (
+					datas?.map((data: TransactionsDatasProps) => (
+						<>
+							<Container>
+								<ContentFirst>
+									<Title>
+										{data.type === "transaction"
+											? "	Transferencia enviada"
+											: "Deposito feito"}
+									</Title>
+									{
+										data.type !== "deposit" ? (
+											<Destiny>
+												Para:
+												<span>
+													{" "}
+													{data.to_who}
+												</span>
+											</Destiny>
+										) : <p style={{ marginTop: "10px" }} />
+									}
+									<Wallet>
+										{new Intl.NumberFormat("pt-BR", {
+											style: "currency",
+											currency: "BRL",
+										}).format(data.value as unknown as number)}
+									</Wallet>
+								</ContentFirst>
+								<ContentSecond>
+									<DateTransaction>
+										{new Intl.DateTimeFormat("pt-BR", {
+											day: "numeric",
+											month: "numeric",
+										}).format(new Date(data.date))}
+									</DateTransaction>
+									<Details>
+										<button type="button" onClick={() => handleOpenModaDetails()}>
+											<BiDetail />
+										</button>
+										<Modal
+											isOpen={isModal}
+											onRequestClose={handleCloseModalDetails}
+											overlayClassName="react-modal-overlay"
+											className="react-modal-content"
+										>
+											<ContainerModalDetails
+											// handleSubmit={() => console.log("")}
+												onCloseModal={handleCloseModalDetails}
+											/>
+										</Modal>
+									</Details>
+								</ContentSecond>
+							</Container>
+						</>
+					))
+				) : (
+					<LoaderComponent />
+				)
+			}
+		</>
+	)
 };
 
 export default InfoListTransactionsAndDeposit;
