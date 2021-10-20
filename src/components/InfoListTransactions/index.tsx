6e2351bb2c43210f588306936582f0332e3d63f1
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-20 20:10:19 -0300

    refactor: style and structure inforLinstTransactions

--- src/components/InfoListTransactions/index.tsx
+++ src/components/InfoListTransactions/index.tsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "react-modal";
+
+// icons
+import { BiDetail } from "react-icons/bi"
+
+// items
+import ContainerModalDetails from "./items";
 
 // interfaces
 import { TransactionsDatasProps } from "../../interface";
 
 // components
-import LoaderExampleLoader from "../Loading";
+import LoaderComponent from "../Loading";
 
 // styles
-import { Container, Content, InfoData } from "./styles";
+import {
+	Container,
+	DateTransaction,
+	Title,
+	Wallet,
+	Destiny,
+	Details,
+	ContentSecond,
+	ContentFirst,
+} from "./styles";
 
 type InfoListTransactionsAndDepositProps = {
   loading: boolean;
@@ -18,49 +34,57 @@ const InfoListTransactionsAndDeposit = ({
 	loading,
 	datas,
 }: InfoListTransactionsAndDepositProps) => {
+	const [isModal, setisModal] = useState(false)
+
+	// handle open modal
+	const handleOpenModaDetails = () => {
+		setisModal(true);
+	};
+
+	// handle close modal
+	const handleCloseModalDetails = () => {
+		setisModal(false);
+	};
+
 	return (
 		<Container>
-			{loading && datas && datas.length > 0 ? (
-				datas?.map((data: TransactionsDatasProps) => (
-					<>
-						<Content>
-							<InfoData>
-								<h4>De quem:</h4>
-								<p>{data.from_who}</p>
-							</InfoData>
-							<InfoData>
-								<h4>Para quem:</h4>
-								<p>{data.to_who}</p>
-							</InfoData>
-							<InfoData>
-								<h4>Data:</h4>
-								<p>
-									{new Intl.DateTimeFormat("pt-BR").format(new Date(data.date))}
-								</p>
-							</InfoData>
-							<InfoData>
-								<h4>Valor:</h4>
-								<p>
-									{new Intl.NumberFormat("pt-BR", {
-										style: "currency",
-										currency: "BRL",
-									}).format(data.value as unknown as number)}
-								</p>
-							</InfoData>
-							<InfoData>
-								<h4>Tipo</h4>
-								<p>
-									{data.to_who ? "Transferencia" : "Deposito"}
-								</p>
-							</InfoData>
-						</Content>
-					</>
-				))
-			) : (
-				<>
-					<LoaderExampleLoader />
-				</>
-			)}
+			<ContentFirst>
+				<Title>Transferencia enviada</Title>
+				<Destiny>
+					Para:
+					<span> Matheus</span>
+				</Destiny>
+				<Wallet>
+					{new Intl.NumberFormat("pt-BR", {
+						style: "currency",
+						currency: "BRL",
+					}).format(10 as unknown as number)}
+				</Wallet>
+			</ContentFirst>
+			<ContentSecond>
+				<DateTransaction>
+					{new Intl.DateTimeFormat("pt-BR", {
+						day: "numeric",
+						month: "numeric",
+					}).format(new Date())}
+				</DateTransaction>
+				<Details>
+					<button type="button" onClick={() => handleOpenModaDetails()}>
+						<BiDetail />
+					</button>
+					<Modal
+						isOpen={isModal}
+						onRequestClose={handleCloseModalDetails}
+						overlayClassName="react-modal-overlay"
+						className="react-modal-content"
+					>
+						<ContainerModalDetails
+							// handleSubmit={() => console.log("")}
+							onCloseModal={handleCloseModalDetails}
+						/>
+					</Modal>
+				</Details>
+			</ContentSecond>
 		</Container>
 	);
 };
--- src/components/InfoListTransactions/styles.ts
+++ src/components/InfoListTransactions/styles.ts
@@ -1,34 +1,66 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  padding: 0 1rem;
-`;
-
-export const Content = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 1rem;
+  color: var(--text-title);
   background-color: var(--background);
-
-  margin: 0 auto 2rem 0;
-  padding: 1rem;
   border-radius: 1rem;
-`;
 
-export const InfoData = styled.div`
+  padding: 0.8rem;
+  margin: 1rem;
+
   display: flex;
-  gap: 0.3rem;
-  text-align: right;
-
-  h4 {
-    margin: 0rem;
-    padding: 0rem;
-    font-size: 15px;
-    font-family: "Poppins";
-  }
+  justify-content: space-between;
 
   p {
-    margin: 0rem;
-    padding: 0rem;
+    margin: 0;
+    padding: 0;
+  }
+  h4 {
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+export const ContentFirst = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Title = styled.p`
+  font-weight: bold;
+  color: var(--text);
+`;
+
+export const Wallet = styled.p`
+  color: var(--text);
+`;
+
+export const Destiny = styled.p`
+  font-size: 13px;
+  span {
+    font-weight: 500;
+    color: var(--text);
+  }
+`;
+
+export const ContentSecond = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  gap: 0.4rem 0;
+`;
+
+export const DateTransaction = styled.p``;
+
+export const Details = styled.div`
+  margin: 0 auto;
+  button {
+    border: none;
+
+    svg {
+      height: 20px;
+      width: 20px;
+      color: var(--text);
+    }
   }
 `;
